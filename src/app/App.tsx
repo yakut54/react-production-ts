5ee5ce3de6bc3aa5edfa1e90fd6e1b8e392e724a
yakut54 <tiub2012@yandex.ru>
2022-12-13 22:09:58 +0700

    sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
 import './styles/index.scss'
 
 const App = () => {
@@ -10,7 +11,10 @@ const App = () => {
 	return (
 		<div className={classNames('app', {}, [theme])}>
 			<Navbar />
-			<AppRouter />
+			<div className="content-page">
+				<Sidebar />
+				<AppRouter />
+			</div>
 		</div>
 	)
 }
