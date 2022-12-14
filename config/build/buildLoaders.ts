02b4b092ed20d756269068dab41dae11a994476a
yakut54 <tiub2012@yandex.ru>
2022-12-14 11:24:41 +0700

    babel & i18next-extract

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -18,6 +18,26 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: ['@svgr/webpack'],
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["ru", "en"],
+              keyAsDefaultValue: true
+            }
+          ]
+        ]
+      }
+    }
+  }
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -44,6 +64,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   return [
     fileLoader,
     svgLoader,
+    babelLoader,
     typescriptLoader,
     cssLoader
   ]
