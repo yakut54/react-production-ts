befd99bd5b46ba02e8151889e0a027adcd4ffd9d
yakut54 <tiub2012@yandex.ru>
2023-01-04 20:22:10 +0700

    authByUsername & TestAsyncThunk _4

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,13 +1,10 @@
-import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit'
+import { configureStore, ReducersMapObject } from '@reduxjs/toolkit'
 import { counterReducer } from 'entities/Counter'
 import { userReducer } from 'entities/User'
 import { createReducerManager } from './reducerManager'
 import { StateSchema } from './StateSchema'
 
-export const createReduxStore = (
-  initialState?: StateSchema,
-  asyncReducers?: ReducersMapObject<StateSchema>,
-) => {
+export const createReduxStore = ( initialState?: StateSchema, asyncReducers?: ReducersMapObject<StateSchema> ) => {
   const rootReducer: ReducersMapObject<StateSchema> = {
     ...asyncReducers,
     counter: counterReducer,
---
+++ src/features/AuthByUserName/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginUsername } from './getLoginUsername';
+
+describe('getLoginError.test', () => {
+    test('should return error', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: 'vasya',
+            },
+        };
+        expect(getLoginUsername(state as StateSchema)).toEqual('vasya');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginUsername(state as StateSchema)).toEqual('');
+    });
+});
