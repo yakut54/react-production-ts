e245694aeb2b0b930abcbce6500e377a80cf8b99
yakut54 <mongodb1@yandex.ru>
2023-01-31 09:58:14 +0700

    TS strict mode 3

--- src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -8,18 +8,10 @@ export const fetchProfileData = createAsyncThunk<
   ThunkConfig<string>
 >('profile/fetchProfileData', async (_, thunkAPI) => {
   const { extra, rejectWithValue } = thunkAPI
-
-  console.log('test 1 >> ', 1)
-
   try {
     const response = await extra.api.get<Profile>('/profile')
-
-    console.log('test 2 >> ', 2)
-
     return response.data
   } catch (err) {
-    console.log('test 3 >> ', 3)
-
     return rejectWithValue('error')
   }
 })
